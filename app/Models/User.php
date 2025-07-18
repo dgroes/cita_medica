ce9a3b3baabe170f0cad4dbb7aa110bc309c3eed
Diego Pasten <diegopasten78@gmail.com>
2025-07-18 14:44:55 -0400

    feat: :hammer: Se añade el método de relaición con el model de Doctor

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,4 +78,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Patient::class);
     }
+
+    public function doctor()
+    {
+        return $this->hasOne(Doctor::class);
+    }
 }

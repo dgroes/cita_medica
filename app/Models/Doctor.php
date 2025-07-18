af231a0b099b3224010b066ae7a1ea2a02f8886c
Diego Pasten <diegopasten78@gmail.com>
2025-07-18 14:11:28 -0400

    feat: :construction_worker: Modelo de Doctor actualizado

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'speciality_id',
+        'medical_license_number',
+        'phone',
+        'biography',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function speciality()
+    {
+        return $this->belongsTo(Speciality::class);
+    }
 }

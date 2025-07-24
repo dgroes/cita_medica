f9cb650294c66411afc0cf02110d8de0eb0a4bac
Diego Pasten <diegopasten78@gmail.com>
2025-07-23 23:26:57 -0400

    feat: :building_construction: Componete de Livewire con la migración de Schedules

---
+++ app/Livewire/Admin/ScheduleManager.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\Doctor;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class ScheduleManager extends Component
+{
+
+    public Doctor $doctor;
+    public $schedule = [];
+    public $days = [
+        1 => 'Lunes',
+        2 => 'Martes',
+        3 => 'Miércoles',
+        4 => 'Jueves',
+        5 => 'Viernes',
+        6 => 'Sábado',
+        0 => 'Domingo' //En Carbon el domingo es de 0
+    ];
+
+    public $apointment_duration = 15;
+    public $intervals;
+
+    //Propiedad computada
+    #[Computed()]
+    public function hourBlocks()
+    {
+        return CarbonPeriod::create(
+            Carbon::createFromTimeString('08:00:00'),
+            '1 hour',
+            Carbon::createFromTimeString('18:00:00')
+        );
+    }
+
+    public function mount()
+    {
+        $this->intervals = 60 / $this->apointment_duration;
+        $this->initialize();
+    }
+
+    public function initialize()
+    {
+        $schedules = $this->doctor->schedules()->get();
+        foreach ($this->hourBlocks as $hourBlock) {
+            $period = CarbonPeriod::create(
+                $hourBlock->copy(),
+                $this->apointment_duration . ' minutes',
+                $hourBlock->copy()->addHour()
+            );
+
+            foreach ($period as $time) {
+
+                foreach ($this->days as $index => $day) {
+                    $this->schedule[$index][$time->format('H:i:s')] = $schedules
+                        ->contains(function ($schedule) use ($index, $time) {
+                            return $schedule->day_of_week == $index && $schedule->start_time->format('H:i:s') == $time->format('H:i:s');
+                        });
+                }
+            }
+        }
+
+        // dd($this->schedule);
+    }
+
+    public function save()
+    {
+        dd($this->schedule);
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.schedule-manager');
+    }
+}
---
+++ database/migrations/2025_07_23_163413_create_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id')->constrained()->onDelete('cascade');
+            $table->unsignedTinyInteger('day_of_week'); // 0-6 for Sunday to Saturday
+            $table->time('start_time'); // Start time of the schedule
+            $table->time('end_time'); // End time of the schedule
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};

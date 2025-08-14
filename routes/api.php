1730571272a82341322c13e8b46d8465e7c47178
Diego Pasten <diegopasten78@gmail.com>
2025-08-13 21:49:32 -0400

    feat: :alien: Se crea Api interna para conexión de pacientes con gestión de cita

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,38 @@
 <?php
 
+use App\Models\Appointment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/* C52: Búsqueda de disponibilidad, selección de horarios y resumen de cita */
+Route::get('/patients', function (Request $request) {
+
+    return User::query()
+        ->select('id', 'name', 'email')
+        ->when(
+            $request->search,
+            fn($query) => $query
+                ->where('name', 'like', "%{$request->search}%")
+                ->orWhere('email', 'like', "%{$request->search}%")
+        )
+        ->when(
+            $request->exists('selected'),
+            /* fn ($query) => $query->whereIn('id', $request->input('selected', [])), */
+            fn($query) => $query->whereHas('patient', function ($query) use ($request) {
+                $query->whereIn('id', $request->input('selected', []));
+            }),
+            fn($query) => $query->limit(10)
+        )
+        ->whereHas('patient')
+        ->with('patient')
+        ->orderBy('name')
+        ->get()
+        ->map(function (User $user) {
+
+            return [
+                'id' => $user->patient->id,
+                'name' => $user->name,
+            ];
+        });
+})->name('api.patients.index');

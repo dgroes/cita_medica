9234161cd3112a2fd04b41ef944ec5524e91a51b
Diego Pasten <diegopasten78@gmail.com>
2025-08-26 21:20:21 -0400

    feat: :art: Index, edit y accion de appointment

---
+++ resources/views/admin/appointments/actions.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center space-x-2">
+    <x-wire-button href="{{ route('admin.appointments.edit', $appointment) }}" yellow xs>
+        <i class="fa-solid fa-pen-to-square"></i>
+    </x-wire-button>
+
+</div>
--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -1,19 +1,20 @@
 <x-admin-layout title="Citas | CitasMédicas" :breadcrumbs="[
-    [
-        'name' => 'Dashboard',
-        'href' => route('admin.dashboard'),
-    ],
-    [
-        'name' => 'Citas',
-    ],
-]">
+        [
+            'name' => 'Dashboard',
+            'href' => route('admin.dashboard'),
+        ],
+        [
+            'name' => 'Citas',
+        ],
+    ]">
 
-<x-slot name="action">
+    <x-slot name="action">
         {{-- C24: Creación de un nuevo registo --}}
         <x-wire-button blue href="{{ route('admin.appointments.create') }}" xs>
             <i class="fa-solid fa-plus"></i>
             Nuevo
         </x-wire-button>
     </x-slot>
+    @livewire('admin.datatables.appointment-table')
 
 </x-admin-layout>

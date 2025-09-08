262bb4fea6d26fb878b87da69a11f4f6201f64c3
Diego Pasten <diegopasten78@gmail.com>
2025-09-08 18:30:35 -0300

    feat: :seedling: Seeder de Permisos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -14,17 +15,56 @@ class RoleSeeder extends Seeder
     /* C21: Semillas (Role) */
     public function run(): void
     {
+        // C61: Roles y permisos
         $roles = [
-            'Paciente',
-            'Doctor',
-            'Administrador',
-            'Recepcionista',
+            'Paciente' => [
+                'access_dashboard',
+                'create_appointment',
+                'read_appointment',
+                'read_calendar',
+            ],
+            'Doctor' => [
+                'access_dashboard',
+                'read_appointment',
+                'update_appointment',
+                'delete_appointment',
+                'read_calendar',
+            ],
+            'Recepcionista' => [
+                'access_dashboard',
+
+                'create_user',
+                'read_user',
+                'update_user',
+                'delete_user',
+
+                'read_paciente',
+                'update_paciente',
+
+                'read_doctor',
+                'update_doctor',
+
+                'create_appointment',
+                'read_appointment',
+                'update_appointment',
+                'delete_appointment',
+
+                'read_calendar',
+
+            ],
         ];
 
-        foreach ($roles as $role) {
+        // Crear roles y asignar permisos
+        foreach ($roles as $role => $permissions) {
             Role::create([
                 'name' => $role
-            ]);
+            ])
+            ->givePermissionTo($permissions);
         }
+
+        //Rol admin con todos los permisos
+        Role::create([
+            'name'=> 'Admin',
+        ])->givePermissionTo(Permission::all());
     }
 }

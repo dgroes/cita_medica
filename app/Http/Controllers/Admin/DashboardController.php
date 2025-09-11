f5e2327f67ea47cebf19328687b4c8eefaac8ea8
Diego Pasten <diegopasten78@gmail.com>
2025-09-11 18:00:40 -0300

    feat: :building_construction: Dashboard para Admin y Recepcionista

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\AppointmentEnum;
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+use App\Models\Doctor;
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -12,6 +17,25 @@ class DashboardController extends Controller
     public function index()
     {
         Gate::authorize('access_dashboard');
-        return view('admin.dashboard');
+
+        $data = [];
+
+        // Total de pacientes
+        $data['total_patients'] = Patient::count();
+
+        // Total de doctores
+        $data['total_doctors'] = Doctor::count();
+
+        // Cantidad de citas de hoy
+        $data['appointments_today'] = Appointment::whereDate('created_at', now())
+            ->where('status', '!=', AppointmentEnum::SCHEDULED)
+            ->count();
+
+        // Ultimos usuarios registrados
+        $data['recent_users'] = User::latest()
+            ->take(5)
+            ->get();
+
+        return view('admin.dashboard', compact('data'));
     }
 }
